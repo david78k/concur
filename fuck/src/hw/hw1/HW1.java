a06684cc95b12ce5638dee6ea23bb320b4a540fd
david78k <david78k@gmail.com>
2012-08-25 11:01:04 -0400

    f

---
+++ fuck/src/hw/hw1/HW1.java
@@ -0,0 +1,13 @@
+package hw.hw1;
+
+public class HW1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HW1");
+	}
+
+}
