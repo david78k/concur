b3b53aa48eeefc78b90be5965a7db65703c9d979
david78k <david78k@gmail.com>
2012-08-26 17:30:02 -0400

    FactorizerServlet added tomcat 7.0 servlet-api.jar referenced

--- src/lectures/lecture2_atomicity/FactorizerServlet.java
+++ src/lectures/lecture2_atomicity/FactorizerServlet.java
@@ -2,6 +2,10 @@ package lectures.lecture2_atomicity;
 
 import java.math.BigInteger;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 public class FactorizerServlet implements Servlet {
 	
 	public void service (ServletRequest req, ServletResponse resp) {  
