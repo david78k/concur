c68f118b87aebad44a3e348427a0cd0cf28f66b8
david78k <david78k@gmail.com>
2012-08-27 11:32:15 -0400

    sp08hw added with P3 and Problems

--- src/lectures/lecture2_atomicity/FactorizerServlet.java
+++ src/lectures/lecture2_atomicity/FactorizerServlet.java
@@ -2,16 +2,29 @@ package lectures.lecture2_atomicity;
 
 import java.math.BigInteger;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+//import javax.servlet.Servlet;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
 
-public class FactorizerServlet implements Servlet {
+//public class FactorizerServlet implements Servlet {
+public class FactorizerServlet {
 	
-	public void service (ServletRequest req, ServletResponse resp) {  
+//	public void service (ServletRequest req, ServletResponse resp) {  
+	public void service (Object req, Object resp) {  
 		BigInteger i = extractFromReq(req);
 		BigInteger [] factors = factor(i);
 		encodeIntoRes(resp,factors);
+	}
+
+	private void encodeIntoRes(Object resp, BigInteger[] factors) {
+	}
+
+	private BigInteger[] factor(BigInteger i) {
+		return null;
+	}
+
+	private BigInteger extractFromReq(Object req) {
+		return null;
 	}
 
 }
---
+++ src/sp08hw1/P3.java
@@ -0,0 +1,29 @@
+package sp08hw1;
+
+//********************************************
+//
+//P3.java
+//
+//Prints a 10x10 character of the letter "M", the initial of my first name.
+//
+//********************************************
+
+public class P3 
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("@@      @@");
+		System.out.println("@@@    @@@");
+		System.out.println("@@@@  @@@@");
+		System.out.println("@@ @@@@ @@");
+		System.out.println("@@  @@  @@");
+		System.out.println("@@      @@");
+		System.out.println("@@      @@");
+		System.out.println("@@      @@");
+		System.out.println("@@      @@");
+		System.out.println("@@      @@");
+
+	}
+}
