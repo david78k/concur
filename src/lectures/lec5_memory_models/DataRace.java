2849128d453447682e7590643393462965f5d73b
david78k <david78k@gmail.com>
2012-09-16 23:27:09 -0400

    DataRace

--- src/lectures/lec5_memory_models/DataRace.java
+++ src/lectures/lec5_memory_models/DataRace.java
@@ -3,15 +3,60 @@ package lectures.lec5_memory_models;
 public class DataRace {
 	
 	// shared variables
-	boolean doen = false;
+	volatile boolean done = false;
+//	boolean done = false;
 	int x;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		new DataRace().start();
 	}
 
+	void start() {
+		// Thread 0
+		new Thread(new Thread0()).start();
+		
+		// Thread 1 
+		new Thread(new Thread1()).start();
+	}
+
+	class Thread0 implements Runnable {
+		public void run() {
+			System.out.println("Thread0");
+			x = f();
+			done = true;
+			
+			System.out.println(done);
+		}
+	}
 	
+	class Thread1 implements Runnable {
+		public void run() {
+			System.out.println("Thread1");
+			
+			while(!done) {
+				/** spin */
+//				try {
+//					Thread.sleep(1);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+			}
+			
+			int y = x;
+			
+			System.out.println(y);
+		}
+	}
+	
+	protected int f() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return 100;
+	}
 }
