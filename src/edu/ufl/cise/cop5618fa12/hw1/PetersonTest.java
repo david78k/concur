fdb8186d10b98467c35e27cf8cc9c329fac8357d
david78k <david78k@gmail.com>
2012-09-19 23:16:30 -0400

    PetersonArray PetersonTest PetersonTwoVar

---
+++ src/edu/ufl/cise/cop5618fa12/hw1/PetersonTest.java
@@ -0,0 +1,12 @@
+package edu.ufl.cise.cop5618fa12.hw1;
+
+public class PetersonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/edu/ufl/cise/cop5618fa12/hw1/PetersonTwoVar.java
@@ -0,0 +1,12 @@
+package edu.ufl.cise.cop5618fa12.hw1;
+
+public class PetersonTwoVar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
