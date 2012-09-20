dc7d9d791febc7eb857014ce76a72527e2dade04
david78k <david78k@gmail.com>
2012-09-20 16:15:20 -0400

    FastArrayList

--- src/edu/ufl/cise/cop5618fa12/hw1/PetersonTest.java
+++ src/edu/ufl/cise/cop5618fa12/hw1/PetersonTest.java
@@ -1,12 +1,42 @@
 package edu.ufl.cise.cop5618fa12.hw1;
 
-public class PetersonTest {
+public class PetersonTest implements HW1Test{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
+	}
+
+	@Override
+	public boolean testLock(HW1Lock lock, int N0, int N1)
+			throws InterruptedException {
+		return false;
+	}
+
+	@Override
+	public boolean testTrylock(HW1Lock lock, int N0, int N1)
+			throws InterruptedException {
+		return false;
+	}
+
+	@Override
+	public boolean testLockInterruptibly(HW1Lock lock, int N0, int N1)
+			throws InterruptedException {
+		return false;
+	}
+
+	@Override
+	public long comparePerformanceSingleThread(HW1Lock lock1, HW1Lock lock2,
+			int N) {
+		return 0;
+	}
+
+	@Override
+	public long comparePerformanceTwoThread(HW1Lock lock0, HW1Lock lock1,
+			int N0, int N1) throws InterruptedException {
+		return 0;
 	}
 
 }
--- src/edu/ufl/cise/cop5618fa12/hw1/PetersonTwoVar.java
+++ src/edu/ufl/cise/cop5618fa12/hw1/PetersonTwoVar.java
@@ -1,12 +1,29 @@
 package edu.ufl.cise.cop5618fa12.hw1;
 
-public class PetersonTwoVar {
+public class PetersonTwoVar implements HW1Lock {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
+	}
+
+	@Override
+	public void lock(int threadID) {
+	}
+
+	@Override
+	public void lockInterruptibly(int threadID) throws InterruptedException {
+	}
+
+	@Override
+	public boolean tryLock(int threadID) {
+		return false;
+	}
+
+	@Override
+	public void unlock(int threadID) {
 	}
 
 }
